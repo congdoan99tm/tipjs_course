a88c0de646c46b66475c7beff17c32194405fe6a
doanlc <doanlc@hocmai.vn>
2023-12-19 21:56:42 +0700

    discount controller

---
+++ src/routes/discount/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const discountController = require('../../controllers/discount.controller');
+const router = express.Router();
+const asyncHandler = require('../../helpers/asyncHandler');
+const { authenticationV2 } = require('../../auth/authUtils');
+
+// get amount a discount
+router.post('/amount', asyncHandler(discountController.getDiscountAmount));
+router.get(
+  '/list_product_code',
+  asyncHandler(discountController.getAllDiscountCodesWithProducts)
+);
+
+// Authentication
+
+router.use(authenticationV2);
+
+router.post('', asyncHandler(discountController.createDiscountCode));
+router.get('', asyncHandler(discountController.getAllDiscountCodes));
+
+module.exports = router;
